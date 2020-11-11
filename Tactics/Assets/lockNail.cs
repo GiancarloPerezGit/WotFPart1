56db8535759720dde9421d06c1a21e89a23a868f
Giancarlo Perez <42480241+GiancarloPerezGit@users.noreply.github.com>
2020-11-10 23:15:20 -0500

    Finished height implement

---
+++ Tactics/Assets/lockNail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lockNail : MonoBehaviour
+{
+    Vector3 t;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        t = transform.localPosition;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = t;
+    }
+}
